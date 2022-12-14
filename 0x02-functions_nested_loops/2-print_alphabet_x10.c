961ddf3ce1ccdaede08bda98061aa9e787048900
root <daphneythokoana@gmail.com>
2022-12-14 15:50:14 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Entry point
+ *
+ * Rturn: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while ( i < 10)
+	{
+		c = 'a';
+
+		while ( c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
