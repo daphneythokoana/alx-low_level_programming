37ab8fd38e6a66fd1453b1e9e35c0653dae0360e
root <daphneythokoana@gmail.com>
2022-12-14 16:01:43 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *int _islower - Entry point
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int _isalpha - Entry point
+ *
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || ((c >= 65 && c <= 90));
+}
