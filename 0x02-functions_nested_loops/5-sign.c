bfd7a9d20c16e6d771b5d5fe806378e19da0665e
root <daphneythokoana@gmail.com>
2022-12-14 16:11:34 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putcha(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return(-1);
+	}
+	else 
+	{
+		_putchar(48);
+		return (0);
+	}
+}
