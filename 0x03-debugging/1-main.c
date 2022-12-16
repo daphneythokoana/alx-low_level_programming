7a032b0bc15f6ef10c5e94c5645f4b555801dbb0
root <daphneythokoana@gmail.com>
2022-12-16 14:30:46 -0800

    debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Infinite loop
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incomming :(\n");
+
+	i = 0;
+
+	printf("infinite loop avoided \\o/\n");
+	return (0);
+}
