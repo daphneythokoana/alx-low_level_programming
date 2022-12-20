446c7e523185f6038f97693f0d34d83562d1cb85
root <daphneythokoana@gmail.com>
2022-12-20 03:06:37 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: This the sting
+ *
+ */
+
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+		;
+	for (index = index - 1; s[index] != '\0'; index--)
+	{
+		_putchar(s[index]);
+	}
+	-putchar('\n');
+}
