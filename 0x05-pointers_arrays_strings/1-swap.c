f226140b06482cbd1278019baaf72fccddad43b2
root <daphneythokoana@gmail.com>
2022-12-20 02:24:59 -0800

    task swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: This is the first entry
+ * @b: This is the second entry
+ *
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int changed;
+
+	changed = *a;
+	*a = *b;
+	*b = changed;
+}
