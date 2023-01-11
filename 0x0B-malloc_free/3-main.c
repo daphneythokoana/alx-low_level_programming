14d8f698920304aab6f78fcf72fd99198bbe9dad
root <daphneythokoana@gmail.com>
2023-01-11 12:28:47 -0800

    task 4

---
+++ 0x0B-malloc_free/3-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int **grid;
+
+	grid = alloc_grid(6, 4);
+	if (grid == NULL)
+	{
+		return (1);
+	}
+	print_grid(grid, 6, 4);
+	printf("\n");
+	grid[0][3] = 98;
+	grid[3][4] = 402;
+	print_grid(grid, 6, 4);
+	return (0);
+}
