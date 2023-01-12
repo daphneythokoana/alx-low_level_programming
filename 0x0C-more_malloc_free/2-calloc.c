c567b84b34c01457afefa14102a5494047149292
root <daphneythokoana@gmail.com>
2023-01-12 12:20:05 -0800

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,6 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+	mem = malloc(size * nmemb);
 	if (mem == NULL)
 		return (NULL);
 	filler = mem;
