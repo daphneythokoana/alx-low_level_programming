73ae4b5b92fe7ccf16c67f4aeb4252808b4eb2cb
root <daphneythokoana@gmail.com>
2023-01-12 11:41:39 -0800

    more_malloc_free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: the number of elemeents
+ * @size: the byte size f each array element
+ *
+ * Return: if nmemb = 0; size = 0; or the function fails - NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	char *filler;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (mem == NULL)
+		return (NULL);
+	filler = mem;
+	for (index = 0; index < (size * nmemb); index++)
+		filler[index] = '\0';
+	return (mem);
+}
---
+++ 0x0C-more_malloc_free/2-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *a;
+
+	a = _calloc(98, sizeof(char));
+	strcpy(a, "Best");
+	strcpy(a + 4, " School! :)\n");
+	a[97] = '!';
+	simple_print_buffer(a, 98);
+	free(a);
+	return (0);
+}
