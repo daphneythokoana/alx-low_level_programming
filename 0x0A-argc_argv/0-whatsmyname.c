88fc3e7e12f02dfa2f77547f93197be0f69c02c4
root <daphneythokoana@gmail.com>
2023-01-13 09:47:49 -0800

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: number of arguments
+ * @argv: array of argments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc_attribte_((unused)), char *argv[])
+{
+	print("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: number of arguments
+ * @argv: array of argments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d/n", argc -1);
+	return (0);
+}
