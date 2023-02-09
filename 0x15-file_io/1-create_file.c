9917a1b625ea2c5b1f7e89a65a474126b826623d
root <daphneythokoana@gmail.com>
2023-02-09 14:48:02 -0800

    file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fed = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,12 +2,14 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <sys/type.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *src, const char *dest);
 
 #endif
